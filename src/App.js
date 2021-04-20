e6862babbfe78ab08c626ea4d03af60338bcc32f
Vinicius <vinicius.camargo@fliper.app>
2021-04-20 18:30:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+  const [location, setLocation] = useState({});
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    const wacthId = navigator.geolocation.watchPosition(handlePositionReceived)
+
+    return () => navigator.geolocation.clearWatch(wacthId)
+  }, []);
+
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords;
+
+    setLocation({ latitude, longitude })
+  };
+
+  useEffect(async () => {
+    const response = await fetch("https://api.github.com/users/vlCoder/repos")
+    const data = await response.json();
+
+    setRepositories(data)
+  }, [])
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} repositorios favoritos`
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositores = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    })
+
+    setRepositories(newRepositores)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>(favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>))}
+      </ul>
+      <br />
+      <p>
+        Latitude: {location.latitude}<br />
+      </p>
+      <p>
+        Logidute: {location.longitude}<br />
+      </p>
+    </>
   );
 }
 
-export default App;
+;
